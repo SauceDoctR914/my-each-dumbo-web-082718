d497babc7c67ff0dd601c4b0a579109db1764cbf
Gavin Soko <sokolofg@gmail.com>
2018-08-17 14:58:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,14 @@
 def my_each(words) 
   if block_given?
-  i = 0 
-  while i < words.length
-    yield(words[i])
-    i = i + 1
+    i = 0 
+    while i < words.length
+      yield(words[i])
+      i = i + 1
+    end
+    words 
   end
-  words 
 end
+
+[1,2,3].each do |n|
+  puts "n + 1 = #{n+1}"
 end
