679b000b265e5fab4c6d77a4c4b9ee7b5ea9f9ef
Gavin Soko <sokolofg@gmail.com>
2018-08-17 15:03:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -10,9 +10,23 @@ def my_each(words)
 end
 
 def my_map(array)
-
+  if block_given?
+    new_arr = []
+    i = 0 
+    while i < aray.length
+      new_arr << array[i] + 1  #yield(array[i])
+      i = i + 1
+    end
+    new_arr 
+  end
 end
 
 [1,2,3].my_each { |n|
   puts "n + 1 = #{n+1}"
 }
+
+
+[1,2,3].my_map { |n|
+  n + 1
+}
+
